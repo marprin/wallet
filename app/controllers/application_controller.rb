a015ddf88ddfcf7c58fa1aea79c28f8a7ff98a47
marprin <marprin93@gmail.com>
2024-10-13 15:20:20 +0700

    add login controller register the endpoint into routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
 
-  def authenticate
-    token = request.headers['Authorization']&.split(' ')&.last
+  # def authenticate
+  #   token = request.headers['Authorization']&.split(' ')&.last
 
-  end
+  # end
 end
