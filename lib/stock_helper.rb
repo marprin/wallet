2aedcbd15b7e0d84841ad3b6d113bda0adbe2705
marprin <marprin93@gmail.com>
2024-10-13 14:42:44 +0700

    update gem style for stock price helper

--- lib/stock_helper.rb
+++ lib/stock_helper.rb
@@ -1,1 +1,20 @@
-require_relative 'stock_helper/stock_helper'
+require 'stock_helper/version'
+require 'uri'
+require 'net/http'
+
+module StockHelper
+  class StockPrice
+    def all_price
+      url = URI.parse("#{RAPID_API_BASE_URL}/any")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true if url.scheme == 'https'
+
+      request = Net::HTTP::Get.new(url, { 'Content-Type' => 'application/json' })
+      request["x-rapidapi-key"] = RAPID_API_HOST
+      request["x-rapidapi-host"] = RAPID_API_HOST
+
+      http.request(request)
+    end
+  end
+end
--- lib/stock_helper/version.rb
+++ lib/stock_helper/version.rb
@@ -1,3 +1,3 @@
-module StockPrice
+module StockHelper
   VERSION = "0.1.0"
 end
