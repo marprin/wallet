aa9878025c4d3fdf8f7b64a06469f7984ca0b524
marprin <marprin93@gmail.com>
2024-10-14 15:43:35 +0700

    change variable type in login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,20 +5,20 @@ class LoginController < ApplicationController
     body = params.require(:login).permit(:email, :password)
     _email = "#{body['email']}".downcase
 
-    @user = User.find_by(email: _email)
-    unless @user
+    user = User.find_by(email: _email)
+    unless user
       return render json: {'error': 'Please make sure your email and/or password correct'}, status: :not_found
     end
 
     # Validate the password
-    pwd_w_salt = "#{body['password']}#{@user.salt}"
-    unless BCrypt::Password.new(@user.password) == pwd_w_salt
+    pwd_w_salt = "#{body['password']}#{user.salt}"
+    unless BCrypt::Password.new(user.password) == pwd_w_salt
       return render json: {'error': 'Please make sure your email and/or password correct'}, status: :not_found
     end
 
     # Generate login token
     payload = {
-      'id': @user.id
+      'id': user.id
     }
 
     token = JwtHelper::Jwt.generate(payload, JWT_EXPIRATION)
