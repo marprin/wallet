69e7bfb5f9d936484037833156b73d23d86f4d81
marprin <marprin93@gmail.com>
2024-10-14 07:36:27 +0700

    register transaction routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   post 'login', to: 'login#login'
+
+  # transaction routes
+  post 'transactions', to: 'transaction#create'
 end
