f646c9aef151d760a922371181c527214a879d27
marprin <marprin93@gmail.com>
2024-10-14 11:20:27 +0700

    add trx type and status constant in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 class Transaction < ApplicationRecord
+  TRX_TYPE_WITHDRAWAL = 1
+  TRX_TYPE_DEPOSIT = 2
+
+  STATUS_FAIL = 1
+  STATUS_DONE = 2
+
   belongs_to :from_id, class_name: 'UserWallet'
   belongs_to :to_id, class_name: 'UserWallet'
 end
