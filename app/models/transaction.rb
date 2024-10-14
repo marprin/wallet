c09efe5f065f7a04e36004f590d12775f0d1141b
marprin <marprin93@gmail.com>
2024-10-14 14:22:30 +0700

    add validation in transaction and user wallet model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,6 +5,10 @@ class Transaction < ApplicationRecord
   STATUS_FAIL = 1
   STATUS_DONE = 2
 
+  validates :amount, presence: true
+  validates :status, presence: true
+  validates :trx_type, presence: true
+
   belongs_to :from_id, class_name: 'UserWallet'
   belongs_to :to_id, class_name: 'UserWallet'
 end
--- app/models/user_wallet.rb
+++ app/models/user_wallet.rb
@@ -1,4 +1,7 @@
 class UserWallet < ApplicationRecord
+  validates :type, presence: true
+  validates :balance, presence: true
+
   belongs_to :user
   has_many :receive_transactions, class_name: 'Transaction', foreign_key: 'from_id'
   has_many :send_transactions, class_name: 'Transaction', foreign_key: 'to_id'
