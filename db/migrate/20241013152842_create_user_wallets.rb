e08516d492ab968b8408e2345669088141836678
marprin <marprin93@gmail.com>
2024-10-14 10:39:11 +0700

    add balance in user wallet

--- db/migrate/20241013152842_create_user_wallets.rb
+++ db/migrate/20241013152842_create_user_wallets.rb
@@ -3,6 +3,7 @@ class CreateUserWallets < ActiveRecord::Migration[7.2]
     create_table :user_wallets do |t|
       t.string :type
       t.references :user, null: false, foreign_key: true
+      t.decimal :balance, precision: 12, scale: 2
 
       t.timestamps
     end
