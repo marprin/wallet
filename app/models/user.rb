c806ae1ea9669113ecfba78ada9a99b02b9e7873
marprin <marprin93@gmail.com>
2024-10-14 14:05:56 +0700

    create relationship for user and user wallet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :salt, presence: true
+
+  has_many :user_wallets
 end
--- app/models/user_wallet.rb
+++ app/models/user_wallet.rb
@@ -2,4 +2,10 @@ class UserWallet < ApplicationRecord
   belongs_to :user
   has_many :receive_transactions, class_name: 'Transaction', foreign_key: 'from_id'
   has_many :send_transactions, class_name: 'Transaction', foreign_key: 'to_id'
+
+  # Scope section
+  scope :internal, -> { where(type: 'InternalWallet') }
+  scope :team, -> { where(type: 'TeamWallet') }
+  scope :stock, -> { where(type: 'StockWallet') }
+
 end
